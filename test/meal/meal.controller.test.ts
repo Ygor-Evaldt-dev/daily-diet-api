bde037c596a05de135a8d992156b34b484d4a364
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-10-02 12:10:12 -0300

    garantido retornar refeições relacionadas ao usuário autenticado e uma refeição específica

--- test/meal/meal.controller.test.ts
+++ test/meal/meal.controller.test.ts
@@ -30,7 +30,7 @@ describe("meal controller", () => {
         execSync("npm run knex migrate:latest");
     });
 
-    it.only("should create a meal to especific user", async () => {
+    it("should create a meal to especific user", async () => {
         await request(app)
             .post("/user")
             .send(bodyToCreateUser);
@@ -41,5 +41,93 @@ describe("meal controller", () => {
             .send(bodyToCreateMeal);
 
         expect(response.status).toBe(HttpStatus.CREATED);
+    });
+
+    it("should update a meal already registred", async () => {
+        await request(app)
+            .post("/user")
+            .send(bodyToCreateUser);
+
+        await request(app)
+            .post("/meal")
+            .set("Authorization", `Basic ${credentials}`)
+            .send(bodyToCreateMeal);
+
+        const responseToGetMeal = await request(app)
+            .get("/meal/0/1")
+            .set("Authorization", `Basic ${credentials}`);
+
+
+        const meal = responseToGetMeal.body.meals[0];
+
+        const responseToUpdateMeal = await request(app)
+            .put(`/meal/${meal.id}`)
+            .set("Authorization", `Basic ${credentials}`)
+            .send({
+                ...bodyToCreateMeal,
+                isOnDiet: false
+            });
+
+        const responseToGetMealById = await request(app)
+            .get(`/meal/${meal.id}`)
+            .set("Authorization", `Basic ${credentials}`);
+
+        expect(responseToUpdateMeal.status).toBe(HttpStatus.OK);
+        expect(responseToGetMealById.body.meal.isOnDiet).toBeFalsy();
+    });
+
+    it("should to find meals from a specific user", async () => {
+        await request(app)
+            .post("/user")
+            .send(bodyToCreateUser);
+
+        await Promise.all([
+            request(app)
+                .post("/meal")
+                .set("Authorization", `Basic ${credentials}`)
+                .send(bodyToCreateMeal),
+            request(app)
+                .post("/meal")
+                .set("Authorization", `Basic ${credentials}`)
+                .send(bodyToCreateMeal)
+        ]);
+
+        const page = 0;
+        const take = 10;
+        const response = await request(app)
+            .get(`/meal/${page}/${take}`)
+            .set("Authorization", `Basic ${credentials}`);
+
+        const { meals, total } = response.body;
+
+        expect(meals.length).toEqual(2);
+        expect(total).toEqual(2);
+        expect(response.body.page).toEqual(page);
+        expect(response.body.take).toEqual(take);
+    });
+
+    it.only("should to find an specific meal", async () => {
+        await request(app)
+            .post("/user")
+            .send(bodyToCreateUser);
+
+        await request(app)
+            .post("/meal")
+            .set("Authorization", `Basic ${credentials}`)
+            .send(bodyToCreateMeal);
+
+        const responseToGetMeal = await request(app)
+            .get("/meal/0/1")
+            .set("Authorization", `Basic ${credentials}`);
+
+        const response = await request(app)
+            .get(`/meal/${responseToGetMeal.body.meals[0].id}`)
+            .set("Authorization", `Basic ${credentials}`);
+
+        const { meal } = response.body;
+        console.log("meal", meal);
+
+        expect(response.status).toEqual(HttpStatus.OK);
+        expect(meal.id).toEqual(responseToGetMeal.body.meals[0].id);
     });
 });
